34c7f0172b5e1b93416afacdfce9f64da60ea6dc
Tristan F <LeoDog896@hotmail.com>
2023-04-26 15:41:48 +0000

    feat: use token from CLI w/ clap

--- liboard/src/main.rs
+++ liboard/src/main.rs
@@ -1,6 +1,15 @@
 use anyhow::Result;
 use lichess_board::LichessClient;
 use futures_util::{pin_mut, StreamExt};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The token to use for the lichess API
+    #[clap(short, long)]
+    token: String,
+}
 
 #[tokio::main]
 async fn main() {
@@ -8,7 +17,9 @@ async fn main() {
 }
 
 async fn main_err() -> Result<()> {
-    let client = LichessClient::new("token");
+    let args = Args::parse();
+
+    let client = LichessClient::new(&args.token);
     let stream = client.stream().await?;
 
     pin_mut!(stream);
